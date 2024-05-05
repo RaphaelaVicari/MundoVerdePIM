1c923d4f5e395f16ca10aed1ab62bfa4511448e5
raphaela <raphaela.silvestre.vicari@gmail.com>
2024-05-05 20:30:37 -0300

    Realizado cadastro de usuário, utilizando regex, vinculado ao banco de dados

--- MundoVerde/Program.cs
+++ MundoVerde/Program.cs
@@ -4,29 +4,142 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp5
 {
     internal class Program
     {
+        static bool ValidarNomeCompleto(string nome)
+        {
+            string pattern = @"^[A-Za-zÀ-ú]+(?:\s[A-Za-zÀ-ú]+)+$";
+            Regex regex = new Regex(pattern);
+
+            return regex.IsMatch(nome);
+        }
+
+        static bool ValidarCPF(String documento)
+        {
+            string pattern = @"^\d{3}\.\d{3}\.\d{3}-\d{2}$";
+            Regex regex = new Regex(@pattern);  
+
+            return regex.IsMatch(documento);
+        }
+
+        static bool ValidarCNPJ(String documento)
+        {
+            string pattern = @"^\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}$";
+            Regex regex = new Regex(@pattern);
+
+            return regex.IsMatch(documento);
+        }
+        static bool ValidarTelefone(String telefone)
+        {
+            string pattern = @"^\(?\d{2}\)?[\s-]?\d{4,5}-?\d{4}$";
+            Regex regex = new Regex(@pattern);
+
+            return regex.IsMatch(telefone);
+        }
+
+        static bool ValidarEmail(String email)
+        {
+            string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            Regex regex = new Regex(@pattern);
+
+            return regex.IsMatch(email);
+        }
+
+        static bool ValidarSenha(String senha)
+        {
+            string pattern = @"^(?=.*[!@#$%^&*()-+=])(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$";
+            Regex regex = new Regex(@pattern);
+
+            return regex.IsMatch(senha);
+        }
+
         static void Main(string[] args)
         {
-   
-            string stringConexao = @"Data Source=localhost;Initial Catalog=Mundo_verde;Integrated Security=True";
-            string consulta = "SELECT * FROM USUARIO";
-
-            using (SqlConnection conexao = new SqlConnection(stringConexao))
+            while (true)
             {
-                SqlCommand comando = new SqlCommand(consulta, conexao);
-                conexao.Open();
+                Console.WriteLine("Digite seu nome completo");
+                string nome = Convert.ToString(Console.ReadLine());
 
-                using (SqlDataReader tabelinha = comando.ExecuteReader())
+                if (!ValidarNomeCompleto(nome.Trim()))
                 {
-                    while (tabelinha.Read())
+                    Console.WriteLine("ERRO: Nome inválido!");
+                    continue;
+                }
+
+                Console.WriteLine("Digite o tipo de documento que deseja cadastrar CPF ou CNPJ");
+                string tipoDocumento = Convert.ToString(Console.ReadLine());
+
+                if (!tipoDocumento.Trim().Equals("CPF", StringComparison.InvariantCultureIgnoreCase) &&
+                    !tipoDocumento.Trim().Equals("CNPJ", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    Console.WriteLine("ERRO: Tipo de documento inválido!");
+                    continue;
+                }
+
+                Console.WriteLine("Digite o número do documento escolhido");
+                string documento = Convert.ToString(Console.ReadLine());
+
+                if (!ValidarCPF(documento.Trim()) && !ValidarCNPJ(documento.Trim()))
+                {
+                    Console.WriteLine("ERRO: Documento informado inválido!");
+                    continue;
+                }
+
+                Console.WriteLine("Digite seu telefone");
+                string telefone = Convert.ToString(Console.ReadLine());
+
+                if (!ValidarTelefone(telefone.Trim()))
+                {
+                    Console.WriteLine("ERRO: Telefone inválido!");
+                    continue;
+                }
+
+                Console.WriteLine("Digite seu e-mail");
+                string email = Convert.ToString(Console.ReadLine());
+
+                if (!ValidarEmail(email.Trim()))
+                {
+                    Console.WriteLine("ERRO: E-mail inválido!");
+                    continue;
+                }
+
+                Console.WriteLine("Digite uma senha com 8 caracteres utilizando um caracter especial");
+                string senha = Convert.ToString(Console.ReadLine());
+
+                if (!ValidarSenha(senha.Trim()))
+                {
+                    Console.WriteLine("ERRO: Senha inválido!");
+                    continue;
+                }
+                
+                string stringConexao = @"Data Source=localhost;Initial Catalog=Mundo_verde;Integrated Security=True";
+                string consulta = "INSERT INTO [dbo].[usuario] (nome, tipo_documento, documento, telefone, email, senha)" +
+                    " VALUES (@nome, @tipoDocumento, @documento,@telefone, @email, @senha)";
+
+                using (SqlConnection conexao = new SqlConnection(stringConexao))
+                {
+                    SqlCommand comando = new SqlCommand(consulta, conexao);
+                    comando.Parameters.AddWithValue("@nome", nome);
+                    comando.Parameters.AddWithValue("@tipoDocumento", tipoDocumento);
+                    comando.Parameters.AddWithValue("@documento", documento);
+                    comando.Parameters.AddWithValue("@telefone", telefone);
+                    comando.Parameters.AddWithValue("@email", email);
+                    comando.Parameters.AddWithValue("@senha", senha);
+                    conexao.Open();
+                    int resultado = comando.ExecuteNonQuery();
+                    if (resultado > 0)
                     {
-                        Console.WriteLine($"ID: {tabelinha["id_usuario"]}, NOME: {tabelinha["NOME"]}, DOCUMENTO: {tabelinha["documento"]}, TIPO: {tabelinha["tipo_documento"]}, E-MAIL: {tabelinha["email"]}, TELEFONE: {tabelinha["telefone"]}, SENHA: {tabelinha["senha"]}");
+                        Console.WriteLine("\nCADASTRO REALIZADO COM SUCESSO!");
+                        Console.ReadLine();
                     }
+
+                    Console.WriteLine(resultado);
                     Console.ReadLine();
+                    break;
                 }
             }
         }
